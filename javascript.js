0fceb433c6a655538f1d6a7668d2f953136af487
szty1 <gerebtamas@gmail.com>
2023-03-07 08:36:57 +0100

    added function to decide round winner

--- javascript.js
+++ javascript.js
@@ -1,27 +1,33 @@
 function getComputerChoice() {
-   let choice;
-   
-   switch(Math.floor(Math.random() * 3)) {
-        case 0:
-            choice = "Rock";
-            break;
-        case 1:
-            choice = "Paper";
-            break;
-        case 2:
-            choice = "Scissors";
-            break;
-        default:
-            choice = "Error!";
-            break;
-   }
+   let choice = ["Rock", "Paper", "Scissors"];
 
-   return choice;
+   return choice[Math.floor(Math.random() * 3)];
 }
 
-function selectWinner(playerSelection, computerSelection){
+function selectWinner(playerSelection, computerSelection) {
 
-}
+    let returnStrin = "Sorry! Something went wrong.";
 
-console.log(getComputerChoice());
+    if (playerSelection === computerSelection) {
+        returnString = `It's a tie! You both chose ${playerSelection}.`;
+    }
+
+    if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
+        (playerSelection === "Paper" && computerSelection === "Rock") || 
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
+            returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+
+    if ((playerSelection === "Rock" && computerSelection === "Paper") || 
+        (playerSelection === "Paper" && computerSelection === "Scissors") || 
+        (playerSelection === "Scissors" && computerSelection === "PRock")) {
+            returnString = `You lost! ${computerSelection} beats ${playerSelection}.`;
+    }
+
+    return returnString;
+}   
+
+
+// console.log(getComputerChoice());
+console.log(selectWinner("Rock", getComputerChoice()));
 
