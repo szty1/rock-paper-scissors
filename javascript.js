021966d3843af63fb945ace13353582f11aa14b1
szty1 <gerebtamas@gmail.com>
2023-03-07 13:10:14 +0100

    added game function moved some functionality to separate functions

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
-   let choice = ["Rock", "Paper", "Scissors"];
+    let choice = ["Rock", "Paper", "Scissors"];
 
-   return choice[Math.floor(Math.random() * 3)];
+    return choice[Math.floor(Math.random() * 3)];
 }
 
 function getPlayerChoice(message = "Pleas enter your choice:") {
@@ -20,37 +20,80 @@ function getPlayerChoice(message = "Pleas enter your choice:") {
     }
 }
 
-function selectWinner(playerSelection, computerSelection) {
+function selectRoundWinner(playerChoice, computerChoice) {
 
-    let returnString = "Sorry! Something went wrong.";
-
-    if (playerSelection === computerSelection) {
-        returnString = `It's a tie! You both chose ${playerSelection}.`;
+    if (playerChoice === computerChoice) {
+            return "tie";
     }
 
-    if ((playerSelection === "Rock" && computerSelection === "Scissors") || 
-        (playerSelection === "Paper" && computerSelection === "Rock") || 
-        (playerSelection === "Scissors" && computerSelection === "Paper")) {
-            returnString = `You win! ${playerSelection} beats ${computerSelection}.`;
+    if ((playerChoice === "Rock" && computerChoice === "Scissors") ||
+        (playerChoice === "Paper" && computerChoice === "Rock") ||
+        (playerChoice === "Scissors" && computerChoice === "Paper")) {
+            return "player";
+        
     }
 
-    if ((playerSelection === "Rock" && computerSelection === "Paper") || 
-        (playerSelection === "Paper" && computerSelection === "Scissors") || 
-        (playerSelection === "Scissors" && computerSelection === "PRock")) {
-            returnString = `You lost! ${computerSelection} beats ${playerSelection}.`;
+    if ((playerChoice === "Rock" && computerChoice === "Paper") ||
+        (playerChoice === "Paper" && computerChoice === "Scissors") ||
+        (playerChoice === "Scissors" && computerChoice === "Rock")) {
+            return "computer";
     }
 
-    return returnString;
-} 
+    return;
+}
+
+function displayResult(winner, playerChoice, computerChoice) {
+
+    if (winner === "player") {
+        console.log(`You win! ${playerChoice} beats ${computerChoice}.`);
+    } else if (winner === "computer") {
+        console.log(`You lost! ${computerChoice} beats ${playerChoice}.`);
+    } else {
+        console.log(`It's a tie! You both chose ${computerChoice}.`);
+    }
+}
+
+function displayScore(scorePlayer, scoreComputer) {
+
+}
+
+function displayWinner(scorePlayer, scoreComputer) {
+
+}
 
 function game(rounds) {
-    for (let i = 0; i < rounds; i++) {
 
+    let computerChoice;
+    let playerChoice;
+    let roundWinner;
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+
+    for (let i = 0; i < rounds; i++) {
+        computerChoice = getComputerChoice();
+        playerChoice = getPlayerChoice();
+
+        while (playerChoice === undefined) {
+            playerChoice = getPlayerChoice("This is not a valid choice. Try again:");
+        }
+
+        roundWinner = selectRoundWinner(playerChoice, computerChoice);
+        displayResult(roundWinner, playerChoice, computerChoice);
+
+        if (roundWinner === "player") {
+            scorePlayer++;
+        } else if (roundWinner === "computer") {
+            scoreComputer++;
+        }
+
+        displayScore(scorePlayer, scoreComputer);
     }
+
+    displayWinner(scorePlayer, scoreComputer);
 
 }
 
 
 // console.log(getComputerChoice());
-console.log(selectWinner(getPlayerChoice(), getComputerChoice()));
+game(5);
 
